54882b97f8f2c6cf2e38a13f496bbc0842078a1f
LeeJaesung2 <dlwotjd9909@naver.com>
2021-05-13 22:14:11 +0900

    1

--- Blog/settings.py
+++ Blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-uj9=sdm)d4akm&x7nwvuxe6(*8dnt_lu!^#2ook^qy+@^*!pv4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
