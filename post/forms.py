e8879cc3fbb67666a074e0cb67c44789662daaa9
LeeJaesung2 <dlwotjd9909@naver.com>
2021-05-13 20:51:03 +0900

    aa

---
+++ post/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Blog
+
+class CreatePostForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title', 'body']
+    # title = forms.CharField(max_length=200)
+    # body = forms.CharField(widget=forms.Textarea)
+
+    # def save(self, *args, **kwargs):
+    #     super(CreatePostForm, self).save(*args, **kwargs)
